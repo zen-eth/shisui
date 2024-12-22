e6b2bedc1b7148ffc29341872fa4f3c7af04963a
fearlessfe <505380967@qq.com>
2024-12-22 20:01:44 +0800

    feat: add log for pebble storage

--- storage/content_storage.go
+++ storage/content_storage.go
@@ -10,8 +10,8 @@ var ErrContentNotFound = fmt.Errorf("content not found")
 var ErrInsufficientRadius = fmt.Errorf("insufficient radius")
 
 var MaxDistance = uint256.MustFromHex("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")
-
-var SizeKey = []byte("size")
+var smallestDistance = uint256.NewInt(0).Bytes32()
+var SizeKey = smallestDistance[:] // smallest key, make sure the key will not be prund
 
 type ContentType byte
 
