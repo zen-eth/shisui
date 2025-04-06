8fa67a30ec7726b2bf5814b1ffb8490bf69ff629
fearlessfe <505380967@qq.com>
2025-04-06 13:13:43 +0800

    feat: add version in client info

--- web3/api.go
+++ web3/api.go
@@ -1,19 +1,11 @@
 package web3
 
 import (
-	"runtime"
-
 	"github.com/zen-eth/shisui/internal/version"
 )
 
 type API struct{}
 
 func (p *API) ClientVersion() string {
-	// TODO add version
-	info, _ := version.VCS()
-	name := "shisui"
-	name += "/" + runtime.GOOS + "-" + runtime.GOARCH
-	name += "/" + runtime.Version()
-	name += "/" + info.Commit + "/" + info.Date
-	return name
+	return version.ClientInfo()
 }
