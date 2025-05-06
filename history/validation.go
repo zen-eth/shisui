ca8271c0ebdb88c224d8469d2b8c32f878b7ff33
zhen peng <505380967@qq.com>
2025-05-06 10:19:12 +0800

    feat: add validation for post deneb proofs (#142)

--- history/validation.go
+++ history/validation.go
@@ -11,11 +11,10 @@ const (
 	SlotsPerEpoch    uint64 = 32
 )
 
-func VerifyPostCapellaHeader(headerHash []byte, proof *BlockProofHistoricalSummariesCapella, historicalSummaries capella.HistoricalSummaries) bool {
-	if !VerifyBeaconBlock(headerHash, proof.GetExecutionBlockProof(), tree.Root(proof.BeaconBlockRoot)) {
+func VerifyCapellaToDenebHeader(headerHash []byte, proof *BlockProofHistoricalSummariesCapella, historicalSummaries capella.HistoricalSummaries) bool {
+	if !VerifyBellatrixToDenebExecutionBlockProof(headerHash, proof.GetExecutionBlockProof(), tree.Root(proof.BeaconBlockRoot)) {
 		return false
 	}
-
 	blockRootIndex := proof.Slot % epochSize
 	genIndex := epochSize + blockRootIndex
 	historicalSummarieIndex := (proof.Slot - CapellaForkEpoch*SlotsPerEpoch) / epochSize
@@ -23,7 +22,18 @@ func VerifyPostCapellaHeader(headerHash []byte, proof *BlockProofHistoricalSumma
 	return merkle.VerifyMerkleBranch(tree.Root(proof.BeaconBlockRoot), proof.GetBeaconBlockProof(), 13, genIndex, historicalSummary)
 }
 
-func VerifyBeaconBlock(headerHash []byte, elProof []tree.Root, root tree.Root) bool {
+func VerifyPostDenebHeader(headerHash []byte, proof *BlockProofHistoricalSummariesDeneb, historicalSummaries capella.HistoricalSummaries) bool {
+	if !VerifyPostDenebExecutionBlockProof(headerHash, proof.GetExecutionBlockProof(), tree.Root(proof.BeaconBlockRoot)) {
+		return false
+	}
+	blockRootIndex := proof.Slot % epochSize
+	genIndex := epochSize + blockRootIndex
+	historicalSummarieIndex := (proof.Slot - CapellaForkEpoch*SlotsPerEpoch) / epochSize
+	historicalSummary := historicalSummaries[historicalSummarieIndex].BlockSummaryRoot
+	return merkle.VerifyMerkleBranch(tree.Root(proof.BeaconBlockRoot), proof.GetBeaconBlockProof(), 13, genIndex, historicalSummary)
+}
+
+func VerifyBellatrixToDenebExecutionBlockProof(headerHash []byte, elProof []tree.Root, root tree.Root) bool {
 	// BeaconBlock level:
 	// - 8 as there are 5 fields
 	// - 4 as index (pos) of field is 4
@@ -37,5 +47,22 @@ func VerifyBeaconBlock(headerHash []byte, elProof []tree.Root, root tree.Root) b
 	// - 12 as pos of field is 12
 	// let gen_index = (gen_index_mid_level * 1 * 16 + 12) = 3228
 	var gIndex uint64 = 3228
-	return merkle.VerifyMerkleBranch(tree.Root(headerHash), elProof, 11, gIndex, root)
+	return merkle.VerifyMerkleBranch(tree.Root(headerHash), elProof, uint64(len(elProof)), gIndex, root)
+}
+
+func VerifyPostDenebExecutionBlockProof(headerHash []byte, elProof []tree.Root, root tree.Root) bool {
+	// BeaconBlock level:
+	// - 8 as there are 5 fields
+	// - 4 as index (pos) of field is 4
+	// let gen_index_top_level = (1 * 8 + 4) = 12
+	// BeaconBlockBody level:
+	// - 16 as there are 12 fields
+	// - 9 as index (pos) of field is 9
+	// let gen_index_mid_level = (gen_index_top_level * 16 + 9) = 201
+	// ExecutionPayload level:
+	// - 32 as there are 17 fields
+	// - 12 as pos of field is 12
+	// let gen_index = (gen_index_mid_level * 32 + 12) = 6444
+	var gIndex uint64 = 6444
+	return merkle.VerifyMerkleBranch(tree.Root(headerHash), elProof, uint64(len(elProof)), gIndex, root)
 }
